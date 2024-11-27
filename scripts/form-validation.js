fd3db5cd38590ecec513d41644a805667808b70e
Gilbert Koomson <gilbertkoomson00@gmail.com>
2024-11-27 11:04:36 -0800

    half done

--- scripts/form-validation.js
+++ scripts/form-validation.js
@@ -17,6 +17,12 @@ function formValidator() {
 	const mortgageAmount=document.getElementById('amount').value;
 	const mortgageTerm=document.getElementById('term').value;
 	const interestRate=document.getElementById('interest').value;
+	const amount=document.getElementById('amount');
+	const term=document.getElementById('term');
+	const rate=document.getElementById('interest');
+	const amountbg=document.getElementById('amountbg');
+	const termbg=document.getElementById('termbg');
+	const ratebg=document.getElementById('interestbg');
 
 	const mortgageTypeError=document.getElementById('type-error');
 	const mortgageAmountError=document.getElementById('amount-error');
@@ -32,14 +38,20 @@ function formValidator() {
 
 	if (validator.isEmpty(mortgageAmount)) {
 		mortgageAmountError.textContent = 'This field is required.';
+		amount.style.borderColor='red'
+		amountbg.style.backgroundColor='red'
 		isValid = false;
 	}
 	if (validator.isEmpty(mortgageTerm)) {
 		mortgageTermError.textContent = 'This field is required.';
+		term.style.borderColor='red'
+		termbg.style.backgroundColor='red'
 		isValid = false;
 	}
 	if (validator.isEmpty(interestRate)) {
 		interestRateError.textContent = 'This field is required.';
+		rate.style.borderColor='red'
+		ratebg.style.backgroundColor='red'
 		isValid = false;
 	}
 	if (!mortgageType) {
